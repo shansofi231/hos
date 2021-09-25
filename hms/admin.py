0b7540b2d80f2b7dedb74d6f23c1dc904b6aa52e
shansofi231 <91050764+shansofi231@users.noreply.github.com>
2021-09-25 13:50:31 +0530

    added files

---
+++ hms/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from.models import Doctor, Patient, Appoinment, AppoinmentDate
+
+admin.site.register(Doctor)
+admin.site.register(Patient)
+admin.site.register(Appoinment)
+admin.site.register(AppoinmentDate)
+
+
+# Register your models here.
